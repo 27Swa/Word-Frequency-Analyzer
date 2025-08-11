f04629f763a56b182531ef56238ba1a75b58ccbc
Sondos Wael <sondos.wael2025@gmail.com>
2025-08-11 21:47:13 +0300

    add word frequency analyzer

---
+++ word_frequency.py
@@ -0,0 +1,38 @@
+# importing needed libraries
+import matplotlib.pyplot as plt
+from collections import Counter
+import re
+
+def handle_read_file():       
+        with open(fpath,"r",encoding="utf-8") as file:
+            for i in file:
+                x = re.findall(r"[a-zA-Z]+",i.lower())
+                cnt.update(x)
+def display_most_frequent():
+        top_words = cnt.most_common(10)
+        word,cnts = zip(*top_words)
+
+        print(f"Word  :  Frequency")
+        for i in range(len(word)):
+            sp_n = 12 - len(word[i])
+            print(f"{word[i]}{' '* sp_n}{cnts[i]}")
+
+        plt.figure(figsize=(12,6))
+        plt.bar(word,cnts)
+        plt.xlabel("Word")
+        plt.ylabel("Frequency")
+        plt.show()
+if __name__ == '__main__':   
+    # Apply word analyzer
+    fpath = input("Enter File Path: ")
+    cnt = Counter()
+
+    try:
+        handle_read_file()
+        display_most_frequent()
+    except FileNotFoundError:
+        print("Wrong File Path Entered!!!")
+    except re.error:
+        print("Error in Regular expression")
+    except:
+        print("There is nothing to display")
